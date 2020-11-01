d09ecf8b31b7a3fe14393acd9412894ea454ebeb
Maxim <SokolovMN123@yandex.ru>
2020-11-01 20:51:09 +0300

    Fix pusher keys

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
+import os
 import pusher
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request, current_app
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 pusher_client = pusher.Pusher(
-  app_id='1097594',
-  key='93c0c9a1854f98ead643',
-  secret='9cceedbc3db379f3e8af',
-  cluster='eu',
-  ssl=True
+    app_id=os.environ.get('PUSHER_ID'),
+    key=os.environ.get('PUSHER_KEY'),
+    secret=os.environ.get('PUSHER_SECRET'),
+    cluster='eu',
+    ssl=True
 )
 
 db = SQLAlchemy(app)
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     message = db.Column(db.String(500))
+
 
 @app.route('/')
 def index():
@@ -36,11 +40,11 @@ def message():
     db.session.add(new_message)
     db.session.commit()
 
-    pusher_client.trigger('chat-channel', 'new-message', {'username': username, 'message': message})
-
+    pusher_client.trigger('chat-channel', 'new-message',
+                          {'username': username, 'message': message})
 
     return ''
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
